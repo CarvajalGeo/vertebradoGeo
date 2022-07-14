45a8bb34ac00f3c904383ea0fe4cd8187735b209
Jorge Carvajal <jorgecarvajalbarreto@gmail.com>
2022-07-13 20:18:27 -0500

    Tiburon is back. Added nombre and clase to code

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,9 +1,9 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Mamifero"); 
+        miVertebrados[0] = new Vertebrado(nombreVertebrado: "Camello", claseVertebrado: "Mamifero"); 
+        miVertebrados[1] = new Vertebrado(nombreVertebrado: "Vaca", claseVertebrado: "Mamifero");
+        miVertebrados[2] = new Vertebrado(nombreVertebrado: "Tiburón", claseVertebrado: "Pez");
         
         return miVertebrados;
     }
